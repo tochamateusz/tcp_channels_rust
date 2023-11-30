8ec64c5fd5e0f36bec2837206efaebad7268475c
toch <tocha.mateusz+main@gmail.com>
2023-11-30 11:22:42 +0100

    feat: remove unused clients

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::collections::HashMap;
 use std::fmt::Display;
 use std::io::Read;
+use std::io::Write;
 use std::net::{TcpListener, TcpStream};
+use std::ops::Deref;
 use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::Arc;
 use std::{result, thread};
 
 type Result<T> = result::Result<T, ()>;
@@ -23,29 +27,101 @@ impl<T: Display> Display for Sensitive<T> {
 }
 
 enum Message {
-    ClientConnected,
-    ClientDisconnected,
-    New(Vec<u8>),
-}
-fn server(_message: Receiver<Message>) -> Result<()> {
-    Ok(())
+    ClientConnected(Arc<TcpStream>),
+    ClientDisconnected(Arc<TcpStream>),
+    New {
+        bytes: Vec<u8>,
+        conn: Arc<TcpStream>,
+    },
 }
 
-fn client(mut stream: TcpStream, messages: Sender<Message>) -> Result<()> {
+#[derive(Debug)]
+struct Client {
+    conn: Arc<TcpStream>,
+}
+
+fn server(messages: Receiver<Message>) -> Result<()> {
+    let mut clients = HashMap::new();
+    loop {
+        let msg = messages.recv().expect("The server reciver is hung up");
+        match msg {
+            Message::ClientConnected(author) => {
+                let addr = author.peer_addr().expect("TODO: cache the peer");
+                let client = Client {
+                    conn: author.clone(),
+                };
+
+                let message = format!(
+                    "Hello client: {}",
+                    client.conn.peer_addr().expect("Lack of addr")
+                );
+
+                client
+                    .conn
+                    .as_ref()
+                    .write(message.as_bytes())
+                    .map_err(|err| eprintln!("ERROR: couldn't write bytest {err}"))?;
+
+                let _inserted = clients.insert(addr, client);
+            }
+            Message::ClientDisconnected(author) => {
+                let addr = author.peer_addr().expect("TODO: cache the peer");
+                clients.remove(&addr);
+            }
+            Message::New { bytes, conn } => {
+                let con_addr = conn.peer_addr().expect("TODO: cache the peer");
+                for (addr, client) in clients.iter() {
+                    if *addr != con_addr {
+                        client
+                            .conn
+                            .as_ref()
+                            .write(&bytes)
+                            .map_err(|err| eprintln!("ERROR: couldn't write bytes {err}"))?;
+                    };
+                }
+            }
+        }
+    }
+}
+
+fn client(stream: Arc<TcpStream>, messages: Sender<Message>) -> Result<()> {
     messages
-        .send(Message::ClientConnected)
+        .send(Message::ClientConnected(stream.clone()))
         .map_err(|err| eprintln!("ERROR: couldn't send message to server thread: {err}"))?;
 
     let mut buffer = vec![0; 64];
 
     loop {
-        let n = stream.read(&mut buffer).map_err(|_| {
-            let _ = messages.send(Message::ClientDisconnected);
+        let n = stream.deref().read(&mut buffer).map_err(|_| {
+            let _ = messages.send(Message::ClientDisconnected(stream.clone()));
         })?;
 
+        if n == 0 {
+            println!("Zero buf");
+            let _ = messages.send(Message::ClientDisconnected(stream.clone()));
+            break Ok(());
+        }
+
+        let client_addr = stream.deref().peer_addr().map_err(|e| {
+            eprintln!("cannot get address:{e}");
+        })?;
+
+        let message_content =
+            String::from_utf8(buffer.clone()).expect("buffer cannot be converter to utf8");
+
+        println!(
+            "Client: {}, filling the buffer:{}",
+            client_addr, message_content
+        );
+
         let _ = messages
-            .send(Message::New(buffer[0..n].to_vec()))
+            .send(Message::New {
+                bytes: buffer[0..n].to_vec(),
+                conn: stream.clone(),
+            })
             .map_err(|err| eprintln!("ERROR: couldn't send message to server thread: {err}"));
+
+        buffer.fill(0);
     }
 }
 
@@ -65,7 +141,7 @@ fn main() -> Result<()> {
             Ok(s) => {
                 println!("{s:#?}");
                 let message_sender = message_sender.clone();
-                thread::spawn(|| client(s, message_sender));
+                thread::spawn(|| client(Arc::new(s), message_sender));
             }
             Err(e) => eprintln!("encounter IO error: {e}"),
         }
